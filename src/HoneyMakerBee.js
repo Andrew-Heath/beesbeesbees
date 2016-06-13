08c29af066c4f68d60448069b5e6d7cf3c8b8c81
andrew-heath <aheath42@gmail.com>
2016-06-13 11:41:27 -0700

    HoneyMakerBee inherits from Bee, has age of 10 and job of 'make honey'

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,9 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
